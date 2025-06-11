659d0edf63af10a9b80050be3a1fd7686c8d6b77
Matteo Hertel <info@matteohertel.com>
2025-06-11 12:36:14 +0100

    fix(nvim):deps

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,4 +5,5 @@ return {
   -- import/override with your plugins folder
   { import = "astrocommunity.recipes.disable-tabline" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
 }
